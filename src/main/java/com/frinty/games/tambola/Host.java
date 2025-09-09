95605405ac6205f0fc566700cc0aa6f16f875431
Architect1604 <architpuri1604@gmail.com>
2025-09-09 22:23:50 +0800

    Creating empty Host class, with comments for next steps

---
+++ src/main/java/com/frinty/games/tambola/Host.java
@@ -0,0 +1,22 @@
+package com.frinty.games.tambola;
+
+public class Host {
+
+	//This class is the host class for playing the game Tambola. 
+	//Once players have tickets from the ticket class, they should be able to play together, which this class enables
+	
+	//This class should call (generate) all the numbers from 1 to 90 (at random)
+	
+	//This class should check (validate) a ticket when a prize is claimed (e.g. house)
+	//This class should then accordingly update the status of the prizes (e.g. house gone/remaining)
+	
+	
+	
+	
+	
+	
+	public Host() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
