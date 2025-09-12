a59477e2da4c0c5f221e5a22b87f52eca186535e
Architect1604 <architpuri1604@gmail.com>
2025-09-12 17:36:52 +0800

    Partial commit - work in progress for call method

--- src/main/java/com/frinty/games/tambola/Host.java
+++ src/main/java/com/frinty/games/tambola/Host.java
@@ -15,11 +15,28 @@ public class Host {
 	//This class should check (validate) a ticket when a prize is claimed (e.g. house)
 	//This class should then accordingly update the status of the prizes (e.g. house gone/remaining)
 	
+	
+	//private boolean house;
+	//private boolean topLine;
+	//private boolean middleLine;
+	//private boolean bottomLine;
+	//private boolean earlyFive;
+	//private boolean fourCorners;
+	
+	//int [] nums = new int [90];
+	//int counter = 1;
+	
+	static ArrayList<Integer> nums = new ArrayList<Integer>();
+	
 	public static void main (String[] args) {
 		Host host = new Host();
-		
-		NumberUtils.printIntArrayList(host.call90());
-		System.out.println("Chosen number is "+host.execute90(89));
+		/*for (int i=0; i<90; i++) {
+			System.out.println("The called number is " + host.call());	
+		}*/
+		host.call();
+		NumberUtils.printIntArrayList(nums);
+		//NumberUtils.printIntArrayList(host.call90());
+		//System.out.println("Chosen number is "+host.execute90(89));
 	}
 	
 	//generate 90 numbers:
@@ -45,6 +62,7 @@ public class Host {
 		return nums;
 	}
 	
+	//TODO modify
 	public int execute90 (int i) {
 		Host host = new Host();
 		ArrayList<Integer> nums = host.call90();
@@ -54,6 +72,58 @@ public class Host {
 		return num;
 	}
 	
+	
+	public int call() {
+		if (nums.size()>90) {
+			nums = new ArrayList<Integer>();
+		}
+		
+		int num = 0;
+		boolean contains = true;
+		
+		while (contains) {
+			//System.out.println("Entered while loop");
+			num = NumberUtils.generateRandom(1, 90);
+			System.out.println("Num is " + num + " and contains is " + contains);
+			for (int i=0; i<nums.size(); i++) {
+				System.out.println(nums.get(i)==num);
+				if (nums.get(i)==num) {
+					//System.out.println("Nums.get(i) condition is " + (nums.get(i)==num));
+					
+					contains = true;
+					break;
+				} else {
+					contains = false;
+				}
+			}
+		}
+		nums.add(num);
+		return num;
+	}
+	
+	/*
+	public int call() {
+		if (counter==91) {
+			nums = new int[90];
+		}
+		
+		int num = 0;
+		
+		boolean contains = true;
+		while (contains) {
+			num = NumberUtils.generateRandom(1, 90);
+			for (int i=0; i<counter; i++) {
+				if (nums[i]==num) {
+					contains = true;
+				} else {
+					contains = false;
+				}
+			}
+			counter++;
+		}
+		
+		return num;
+	}*/
 	
 	public Host() {
 		// TODO Auto-generated constructor stub
